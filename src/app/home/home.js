3cb19d22f18a38bf0a7a8fa0cc0c73cc7975db33
Bastian Gruber <coding@bastiangruber.de>
2015-03-20 11:58:49 +0100

    numbers are working

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -39,6 +39,8 @@ angular.module( 'CalculateYourLife.home', [
  */
 .controller('HomeCtrl', ['$scope','Income', 'Outcome', function HomeController( $scope, Income, Outcome ) {
   console.log("inside HomeCtrl");
+  $scope.hours = 0;
+  $scope.income_per_hour = 0;
   $scope.income = {};
   $scope.outcome = {};
   $scope.expenses = [];
@@ -47,11 +49,35 @@ angular.module( 'CalculateYourLife.home', [
     var income = new Income($scope.income);
     $scope.income.income_per_hour = Math.round(income.income_per_hour).toFixed(2);
     $scope.disable_income = true;
+    $scope.sum_income();
+    $scope.sum_hours();
   };
 
   $scope.addOutcome = function() {
     var outcome = new Outcome($scope.outcome);
     $scope.expenses.push(outcome);
+    $scope.sum_income();
+    $scope.sum_hours();
+  };
+
+  $scope.sum_hours = function() {
+    var sum_hours = 0;
+    $scope.expenses.forEach(function(element) {
+      sum_hours += element.hours_per_week;
+    });
+
+    $scope.hours = sum_hours + $scope.income.hours_per_week;
+  };
+
+  $scope.sum_income = function() {
+    var sum_outcome = 0;
+    var income_per_week = $scope.income.income_per_hour * $scope.income.hours_per_week;
+
+    $scope.expenses.forEach(function(element) {
+      sum_outcome += element.cost_per_week;
+    });
+
+    $scope.income_per_hour = (income_per_week - sum_outcome) / $scope.income.hours_per_week;
   };
 
 }])
